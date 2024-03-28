1fa360eb0e8590f8e6df6d334c26d5ad653cb456
prabhattiwarii <prabhattiwari9629@gmail.com>
2024-03-28 16:39:59 +0530

    footer done

--- Components/layout/Footer.js
+++ Components/layout/Footer.js
@@ -1,48 +1,39 @@
 import React from 'react'
 import style from "./Footer.module.css";
-
-
-
 const Footer = () => {
-  return (
-
-    <div className={style.mainsection}>
-    <div className={style.mainwrap}>
-
-
-<div className={style.wraptestimonial}>
-<div className={style.mainwrap1}>C-J Technology</div>
-<div className={style.mainwrap2}>C-J Technology is committed to delivering innovative, qualitative, and customized IT solutions using vast experience and Superior skills for its Happy Clients.</div>
-</div>
-
-
-<div className={style.wraptestimonial1}>
-<div className={style.mainwrapmain}>IT Services</div>
-<div className={style.colorlist}>
-<li className={style.mainwrap1}>Mobile App Development</li>
-<li className={style.mainwrap1}>Web Developer</li>
-<li className={style.mainwrap1}>UI/UX Design</li>
-<li className={style.mainwrap1}>Application Maintenance & Support</li>
-<li className={style.mainwrap1}>Hire Dedicated Developers</li>
-</div>
-
-</div>
-
-<div className={style.wraptestimonial1}>
-<div className={style.mainwrapmain}>Contact Info</div>
-<div className={style.wrap}>
-<div className={style.mainwrap1}>Near ,Sugar-Complex<br/> 709 A-D National Highway Delhi-NCR</div>
-<div className={style.mainwrap1}>jobiskille@gmail.com</div>
-
-<div className={style.mainwrap2}>Monday to Friday -09:00am - 6:00pm</div>
-</div>
-</div>
- </div>
-
-<div className={style.Bottom}>Copyright &copy; {new Date().getFullYear()} <a href="/" className={style.Bottom1} style={{fontWeight:600}}><span style={{color:"skyblue" }}>&nbsp; Coding Jobs </span> <span style={{color:"skyblue"}}>Technologies &nbsp;</span></a> Pvt. Ltd. | All rights Reserved.</div>
-</div>
-
-  )
+    return (
+        <div className={style.footerwrap}>
+            <div className={style.footertop}>
+                <div className={style.footerdetails}>
+                    <div className={style.detailsheading}><a href="#">C-J Technology</a></div>
+                    <div className={style.text}>C-J Technology is committed to delivering innovative, qualitative, and customized IT solutions using vast experience and Superior skills for its Happy Clients.</div>
+                </div>
+                <div className={style.footerdetails}>
+                    <div className={style.detailsheading}>IT Services</div>
+                    <ul className={style.footerlists}>
+                        <li className={style.footerlist}><a href="#">Mobile App Development</a></li>
+                        <li className={style.footerlist}><a href="#">Web Developer</a></li>
+                        <li className={style.footerlist}><a href="#">UI/UX Design</a></li>
+                        <li className={style.footerlist}><a href="#">Application Maintenance & Support</a></li>
+                        <li className={style.footerlist}><a href="#">Hire Dedicated Developers</a></li>
+                    </ul>
+                </div>
+                <div className={style.footerdetails}>
+                    <div className={style.detailsheading}>Contact Info</div>
+                    <ul className={style.contactlist}>
+                        <li className={style.address}>Near ,Sugar-Complex<br /> 709 A-D National Highway Delhi-NCR</li>
+                        <li className={style.email}>jobiskille@gmail.com</li>
+                        <li className={style.timing}>Monday to Friday <br />09:00am - 6:00pm</li>
+                    </ul>
+                </div>
+            </div>
+            <div className={style.footerbottom}>
+                <div className={style.footercopyright}>
+                    Copyright &copy; {new Date().getFullYear()} <a href="/" className={style.Bottom1} style={{ fontWeight: 600 }}><span style={{ color: "skyblue" }}>&nbsp; Coding Jobs </span> <span style={{ color: "skyblue" }}>Technologies &nbsp;</span></a> Pvt. Ltd. | All rights Reserved.
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Footer;
